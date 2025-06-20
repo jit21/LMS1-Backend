759424e42f0bec16685825ee314819ca29a1b9d5
Jit Das <129063139+jit21@users.noreply.github.com>
2025-06-20 16:25:41 +0530

    Update auth.middleware.js-1

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,22 +2,44 @@ import AppError from "../utils/error.util.js";
 import jwt from "jsonwebtoken"
 
 const isLoggedIn = async (req, res, next) => {
-    const { token } = req.cookies;
-    console.log("request in cookies",req.cookies);
-    console.log("token in is logged in",token);
+    // const { token } = req.cookies;
+    // console.log("request in cookies",req.cookies);
+    // console.log("token in is logged in",token);
 
-    if (!token) {
+    // if (!token) {
+    //     return res.status(401).json({
+    //         success: false,
+    //         message: "Login first to access this resource",
+    //     });
+    // }
+
+    // try {
+    //     const userDetails = jwt.verify(token, process.env.JWT_SECRET);
+    //     req.user = userDetails; // ✅ Attach user info to request
+    //     console.log(" No Error in isLoggedin");
+
+    //     next();
+    // } catch (err) {
+    //     return res.status(401).json({
+    //         success: false,
+    //         message: "Invalid or expired token",
+    //     });
+    // }
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res.status(401).json({
             success: false,
-            message: "Login first to access this resource",
+            message: "Authorization header missing or invalid",
         });
     }
 
+    const token = authHeader.split(" ")[1];
+
     try {
         const userDetails = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = userDetails; // ✅ Attach user info to request
-        console.log(" No Error in isLoggedin");
-
+        req.user = userDetails;
+        console.log("Authenticated user:", userDetails);
         next();
     } catch (err) {
         return res.status(401).json({
